320631984ae273fbb9e0fb75c36d6918103a7300
tylerdukedev <tylerdukedev@gmail.com>
2023-12-20 21:50:11 -0500

    Add project files.

---
+++ VampireAscension/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+public class Player
+{
+    private int _hp;
+    private string _name = string.Empty;
+    public List<string> _inventory = new List<string>();
+
+    public Player(int hp, string name)
+    {
+        _hp = hp;
+        _name = name;
+    }
+
+    public void InventoryAdd(string item)
+    {
+        _inventory.Add(item);
+    }
+
+    public void InventoryRemove()
+    {
+
+    }
+
+}
